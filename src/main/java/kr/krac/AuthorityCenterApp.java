88747677921beb7351926ed320cce093f265d753
Oleg Anastassov <oleg_anastassov@epam.com>
2024-02-20 15:21:56 +0100

    Add walking skeleton

---
+++ src/main/java/kr/krac/AuthorityCenterApp.java
@@ -0,0 +1,13 @@
+package kr.krac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityCenterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityCenterApp.class, args);
+    }
+
+}
